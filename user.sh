183ee38a7b2ca96e50080d818e77a8ff40c1d164
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-03 13:23:57 -0500

    user configuration done.

---
+++ user.sh
@@ -0,0 +1,16 @@
+cp user.service /etc/systemd/system/user.service
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+
