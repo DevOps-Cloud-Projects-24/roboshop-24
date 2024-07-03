ae601f76aa845a76e90f69c985c1bd658bcc41dd
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-03 12:54:10 -0500

    nginx configuration done.

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+cp nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp nginx.conf /etc/nginx/nginx.conf
 systemctl restart nginx
