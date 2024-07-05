f75d51d601a7e4b3cae0cefc137cdbd75fc4570c
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-05 12:06:29 -0500

    SED configuration done.

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-# update config file
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
