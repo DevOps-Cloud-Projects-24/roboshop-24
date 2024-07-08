485fc9c20da6556ea54da4cd1d15563b2414aa4a
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-08 11:26:37 -0500

    cart configuration done.

--- cart.sh
+++ cart.sh
@@ -1,10 +1,10 @@
-cp cart.service /etc/systemd/system/user.cart
+cp cart.service /etc/systemd/system/cart.service
 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
-rm -rf /app
+rm -rf /App
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
@@ -13,5 +13,6 @@ cd /app
 npm install
 systemctl daemon-reload
 systemctl enable cart
-systemctl restart cart
+systemctl start cart
+
 
