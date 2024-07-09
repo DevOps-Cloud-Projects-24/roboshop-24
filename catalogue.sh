8c1509434aab3b14065f99ff5e5a77b00e50f438
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-09 15:06:26 -0500

    shell scripting directors for output

--- catalogue.sh
+++ catalogue.sh
@@ -5,14 +5,6 @@ NODEJS
 
 
 dnf install mongodb-mongosh -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 mongosh --host mongo.dev.idevops24.online </app/db/master-data.js
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
--- common.sh
+++ common.sh
@@ -6,125 +6,72 @@ PRINT(){
   echo "######################## *$ ##################" &>>$log_file
   echo $*
 }
+STAT(){
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+}
 
 NODEJS(){
 PRINT disable node nodejs # This is how you can able to print only the optional output
 dnf module disable nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT enable nodejs
 dnf module enable nodejs:20 -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT install nodejs
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT copy service file
 cp ${component}.service /etc/systemd/system/${component}.service &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT copy mongo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT adding user
 useradd roboshop &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT removing app directory
 rm -rf /app &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
  PRINT Making new directory
 mkdir /app &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT downloading the zip file
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$log_file
 cd /app
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT Unzipping the file
 unzip /tmp/${component}.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 cd /app
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT Installing packages
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT reloading
 systemctl daemon-reload &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT enabling the server
 systemctl enable ${component} &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 PRINT restarting the server
 systemctl restart ${component} &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 }
